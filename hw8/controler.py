cb64c4903fb3834b5d3191ef00472437c39a9c18
luckydn <sp.donbass@gmail.com>
2023-03-02 04:44:18 +0300

    hw8

---
+++ hw8/controler.py
@@ -0,0 +1,21 @@
+from menu_contact import menu
+
+choice = menu()
+
+match choice:
+    case 1:
+        pass
+    case 2:
+        pass
+    case 3:
+        pass
+    case 4:
+        pass
+    case 5:
+        pass
+    case 6:
+        pass
+    case 7:
+        pass
+    case 8:
+        pass
