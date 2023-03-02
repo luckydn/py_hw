705980c4784c7f93110b4387b65cb37956e3c594
luckydn <sp.donbass@gmail.com>
2023-03-03 00:09:42 +0300

    hw8

--- hw8/controler.py
+++ hw8/controler.py
@@ -1,21 +1,41 @@
-from menu_contact import menu
+import db_manager
+import menu_contact
 
-choice = menu()
 
-match choice:
-    case 1:
-        pass
-    case 2:
-        pass
-    case 3:
-        pass
-    case 4:
-        pass
-    case 5:
-        pass
-    case 6:
-        pass
-    case 7:
-        pass
-    case 8:
-        pass
+def start():
+    while True:
+        choise = menu_contact.menu()
+        match choise:
+            case 1:
+                db_manager.open_file()
+            case 2:
+                db_manager.safe_file()
+            case 3:
+                pb = db_manager.get()
+                menu_contact.show_contacts(pb)
+            case 4:
+                new = menu_contact.new_contact_input()
+                db_manager.add(new)
+            case 5:
+                find = menu_contact.search_contact()
+                result = db_manager.serch(find)
+                menu_contact.show_contacts(result)
+            case 6:
+                pb = db_manager.get()
+                menu_contact.show_contacts(pb)
+                ind = menu_contact.input_id()
+                contact = menu_contact.new_contact_input()
+                db_manager.change_contact(ind, contact)
+            case 7:
+                pb = db_manager.get()
+                menu_contact.show_contacts(pb)
+                ind = menu_contact.input_id()
+                name = db_manager.get_name(ind)
+                if menu_contact.confirm('Удалить', name):
+                    db_manager.delete_contact(ind)
+            case 8:
+                if db_manager.check_changes():
+                    if menu_contact.confirm_changes():
+                        db_manager.safe_file()
+                print("До свидания")
+                break
--- hw8/menu_contact.py
+++ hw8/menu_contact.py
@@ -10,8 +10,50 @@ def menu():
         6. изменить контакт
         7. удалить контакт
         8. выход''')
-    choise = int(input("Введите пункт меню: "))
-    return choise
+    while True:
+        try:
+            choise = int(input("Введите пункт меню: "))
+            if 0 < choise < 9:
+                return choise
+            else:
+                print("Введите число от 1 до 8")
+        except:
+            print("Вводите цифры а не буквы")
 
+def show_contacts(pb: list[dict]):
+    if pb == []:
+        print("Телефонная книга пуста или файл не открыт")
+    else:
+        for i, contact in enumerate(pb, 1):
+            name = contact.get('name')
+            phone = contact.get('phone')
+            comment = contact.get('comment')
+            print(f'{i}. {name:<20} {phone:<20} {comment:<20}')
 
-menu()
+def new_contact_input():
+    name = input("Введите имя и фамилию: ")
+    phone = input("Введите номер телефона: ")
+    comment = input("Введите комментарий: ")
+    new_contact = {'name': name,
+                'phone': phone,
+                'comment': comment}
+    return new_contact
+
+def search_contact():
+    search = input("Введите искомый эллемент: ")
+    return search
+
+def input_id():
+    ind = int(input("Введите индекс: "))
+    return ind
+
+def confirm(condition: str, name: str):
+    answer = input(f"Вы действительно хотите {condition} контакт {name}? y/n?: ")
+    if answer == 'y':
+        return True
+    else:
+        return False
+
+def confirm_changes():
+    answer = input("У вас есть не сохраненные изменения, хотите их сохранить? y/n: ")
+    return True if answer == 'y' else False
